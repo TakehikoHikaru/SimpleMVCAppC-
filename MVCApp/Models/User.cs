5af092f18a1c5097564b5fd46983e346b852a6a3
Guilherme Passos <dospassosguilherme@gmail.com>
2022-06-28 21:03:19 -0300

    Add UserModel

---
+++ MVCApp/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCApp.Models
+{
+    public class User
+    {
+        [Key]
+        public String Id { get; set; }
+        [Required(ErrorMessage = "Nome é um campo obrigatório")]
+        public String Name { get; set; }
+        public String Company { get; set; }
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Email não é valido")]
+        public String Email { get; set; }
+        public int PersonalPhoneDDD { get; set; }
+        [MaxLength(3)]
+        public int PersonalPhoneDDI { get; set; }
+        public int PersonalPhoneNumber { get; set; }
+        public int CommercialPhoneDDD { get; set; }
+        [MaxLength(3)]
+        public int CommercialPhoneDDI { get; set; }
+        public int CommercialPhoneNumber { get; set; }
+
+    }
+}
