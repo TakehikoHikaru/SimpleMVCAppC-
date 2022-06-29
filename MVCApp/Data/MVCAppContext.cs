c0e65b577842434ad8b24672baebe8052755a2e1
Guilherme Passos <dospassosguilherme@gmail.com>
2022-06-28 21:29:47 -0300

    Add User and Email Controller and views

---
+++ MVCApp/Data/MVCAppContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCApp.Models;
+
+namespace MVCApp.Data
+{
+    public class MVCAppContext : DbContext
+    {
+        public MVCAppContext (DbContextOptions<MVCAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCApp.Models.User> User { get; set; }
+
+        public DbSet<MVCApp.Models.Email> Email { get; set; }
+    }
+}
---
+++ MVCApp/Migrations/20220629002443_InitalMigration.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCApp.Migrations
+{
+    public partial class InitalMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CommercialPhone",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    DDD = table.Column<int>(type: "int", nullable: false),
+                    DDI = table.Column<int>(type: "int", nullable: false),
+                    Number = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CommercialPhone", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonalPhone",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    DDD = table.Column<int>(type: "int", nullable: false),
+                    DDI = table.Column<int>(type: "int", nullable: false),
+                    Number = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonalPhone", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Company = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PersonalPhoneId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    CommercialPhoneId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_User_CommercialPhone_CommercialPhoneId",
+                        column: x => x.CommercialPhoneId,
+                        principalTable: "CommercialPhone",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_User_PersonalPhone_PersonalPhoneId",
+                        column: x => x.PersonalPhoneId,
+                        principalTable: "PersonalPhone",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Email",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    EmailAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Email", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Email_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Email_UserId",
+                table: "Email",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_CommercialPhoneId",
+                table: "User",
+                column: "CommercialPhoneId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_PersonalPhoneId",
+                table: "User",
+                column: "PersonalPhoneId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Email");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "CommercialPhone");
+
+            migrationBuilder.DropTable(
+                name: "PersonalPhone");
+        }
+    }
+}
