f8d8377a513c258c865f6b52f47b5d9941f11b2d
Guilherme Passos <dospassosguilherme@gmail.com>
2022-07-01 04:19:17 -0300

    Reverting

---
+++ MVCApp/Models/CollectData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCApp.Models
+{
+    public class CollectData
+    {
+
+        public List<User> Users { get; set; }
+        public List<Email> Emails { get; set; }
+
+    }
+}
